1f2027bb51b719a4d549dd00cda71148bf6f89dd
Eugene Alyoshin <aloyolreal@gmail.com>
2024-07-07 06:43:14 +0700

    добавил courseLogoSrc

---
+++ src/lib/courseLogoSrc.ts
@@ -0,0 +1,7 @@
+export const courseLogoSrc = {
+    BodyFlex: "/punlic/images/images_small/bodyflex_female_sm.png",
+    StepAerobic: "/punlic/images/images_small/step_aerobic_sm.png",
+    Stretching: "/punlic/images/images_small/stretching_female_sm.png",
+    Yoga: "/punlic/images/images_small/yoga_female_sm.png",
+    Zumba: "/punlic/images/images_small/zumba_female_sm.png",
+}
