615732cb23812ef415822d5d8fef7820a34a03ae
Eugene Alyoshin <aloyolreal@gmail.com>
2024-07-07 06:45:14 +0700

    исправлена ошибка

--- src/lib/courseLogoSrc.ts
+++ src/lib/courseLogoSrc.ts
@@ -1,7 +1,7 @@
 export const courseLogoSrc = {
-    BodyFlex: "/punlic/images/images_small/bodyflex_female_sm.png",
-    StepAerobic: "/punlic/images/images_small/step_aerobic_sm.png",
-    Stretching: "/punlic/images/images_small/stretching_female_sm.png",
-    Yoga: "/punlic/images/images_small/yoga_female_sm.png",
-    Zumba: "/punlic/images/images_small/zumba_female_sm.png",
+    BodyFlex: "/public/images/images_small/bodyflex_female_sm.png",
+    StepAerobic: "/public/images/images_small/step_aerobic_sm.png",
+    Stretching: "/public/images/images_small/stretching_female_sm.png",
+    Yoga: "/public/images/images_small/yoga_female_sm.png",
+    Zumba: "/public/images/images_small/zumba_female_sm.png",
 }
