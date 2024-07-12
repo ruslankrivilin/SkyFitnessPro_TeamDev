92eb4477f62a38e0eb63451157bfdaf76d207287
Igor Shipitko <garikrhip20@gmail.com>
2024-07-12 18:54:43 +0300

    cours подготовка страницы курса

--- src/pages/CoursePage/CoursePage.tsx
+++ src/pages/CoursePage/CoursePage.tsx
@@ -1,6 +1,15 @@
 import Header from "../../components/Common/Header/Header";
-
+import CourseLogo from "../../components/DataComponents/CourseLogo/CourseLogo";
+import Directions from "../../components/DataComponents/Directions/Directions";
+import FittingText from "../../components/DataComponents/FittingText/FittingTexts";
 
 export default function CoursePage() {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <CourseLogo />
+      <FittingText />
+      <Directions />
+    </>
+  );
 }
