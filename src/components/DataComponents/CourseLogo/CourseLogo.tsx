83d39a67f7a8eb2033d2598b1e2f5174bfbd1954
Igor Shipitko <garikrhip20@gmail.com>
2024-07-12 18:52:31 +0300

    cours верстка готова

--- src/components/DataComponents/CourseLogo/CourseLogo.tsx
+++ src/components/DataComponents/CourseLogo/CourseLogo.tsx
@@ -1,3 +1,21 @@
 export default function CourseLogo() {
-  return;
+  return (
+    <>
+      <div className="py-l">
+        <div className="my-l container mx-auto rounded-blockRadius bg-yogaColor">
+          <div className="marker p-30 mx-auto flex h-80 items-start gap-x-32">
+            <h2 className="p-xl font-defaultFont text-7xl font-normal text-white">
+              Йога
+            </h2>
+            <div className="h-full w-full pl-[650px]">
+              <img
+                className="h-full object-contain object-center"
+                src="/images/images_full/yoga_female_hd_slice.png"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
