ccb1a25e7125043ec43e15f133ac3175ae6286ce
Elizaveta810 <elizaveta.k810@mail.ru>
2024-07-14 19:52:45 +0300

    Доработала логику components_Header_UserModal

--- src/components/Common/Header/Header.tsx
+++ src/components/Common/Header/Header.tsx
@@ -1,11 +1,16 @@
 import { useEffect, useState } from "react";
 import "../../../css/style.css";
+import SigninForm from "../SigninForm/SigninForm";
+import UserModal from "../UserModal/UserModal";
 
 // import UserModal from "../UserModal/UserModal";
 
-export default function Header() {
+export default function Header(page: string) {
   const [isAuthorizated, setIsAuthorizated] = useState<boolean>(false);
-  const [isOpenSigninForm, setIsOpenSigninForm] = useState<boolean>(false);
+  const [isOpenedSigninForm, setIsOpenedSigninForm] = useState<boolean>(false);
+  const [isOpenedUserModal, setIsOpenedUserModal] = useState<boolean>(false);
+  const [isCorrectForTextPage, setIsCorrectForTextPage] =
+    useState<boolean>(true);
 
   useEffect(() => {
     const user = localStorage.getItem("user");
@@ -16,13 +21,20 @@ export default function Header() {
     }
   }, []);
 
-  function handleOpenSigninForm () {
-    setIsOpenSigninForm(true);
-  }
-  // function handleUserModal = () => {
-  //   setIsOpenedUserModal(prevState => !prevState);
-  // };
+  useEffect(() => {
+    if (page === "MainPage" || page === "CoursePage") {
+      setIsCorrectForTextPage(true);
+    } else {
+      setIsCorrectForTextPage(false);
+    }
+  }, [page]);
 
+  function handleOpenSigninForm() {
+    setIsOpenedSigninForm(true);
+  }
+  function handleUserModal() {
+    setIsOpenedUserModal((prevState) => !prevState);
+  }
 
   return (
     <div className="mx-auto mt-[50px] flex items-center justify-between">
@@ -32,33 +44,41 @@ export default function Header() {
             <img src="/images/logo.png" alt="logo" />
           </a>
         </div>
-        <div className="space-y-[60px]">
-          <p className="font-roboto hidden md:flex">
-            Онлайн тренировки для занятий дома
-          </p>
-        </div>
+        {isCorrectForTextPage && (
+          <div className="space-y-[60px]">
+            <p className="font-roboto hidden md:flex">
+              Онлайн тренировки для занятий дома
+            </p>
+          </div>
+        )}
       </div>
 
-      {!isAuthorizated && (<button className="h-[52px] w-[103px] rounded-buttonRadius bg-mainColor px-btnX py-btnY text-center font-defaultFont font-normal text-black hover:bg-mainHover"
-      onClick={handleOpenSigninForm}>
-        Войти
-      </button>)}
-
       {/* Если пользователь авторизован, то user Блок */}
-       {isAuthorizated && (<div className="flex items-center ">
-        <svg className="w-[50px] h-[50px] m-[16px]">
-          <use xlinkHref="./public/icons/sprite.svg#icon-profile" />
-        </svg>
-
-        <div className="ml-2 flex items-center" >
-          <div className="mr-[12px]">Сергей</div>
-          <svg className="w-[8px] h-[8px]">
-            <use xlinkHref="./public/icons/sprite.svg#icon-user-arrow" />
+      {isAuthorizated ? (
+        <div className="flex items-center">
+          <svg className="m-[16px] h-[50px] w-[50px]">
+            <use xlinkHref="./public/icons/sprite.svg#icon-profile" />
           </svg>
+
+          <div className="ml-2 flex items-center" onClick={handleUserModal}>
+            <div className="mr-[12px]">Сергей</div>
+            <svg className="h-[8px] w-[8px]">
+              <use xlinkHref="./public/icons/sprite.svg#icon-user-arrow" />
+            </svg>
+          </div>
         </div>
-      </div> )}
-      {isOpenSigninForm && <SigninForm />}
-      {/* <UserModal /> */}
+      ) : (
+        <button
+          className="h-[52px] w-[103px] rounded-buttonRadius bg-mainColor px-btnX py-btnY text-center font-defaultFont font-normal text-black hover:bg-mainHover"
+          onClick={handleOpenSigninForm}
+        >
+          Войти
+        </button>
+      )}
+      {isOpenedSigninForm && (
+        <SigninForm setIsOpenedSigninForm={setIsOpenedSigninForm} />
+      )}
+      {isOpenedUserModal && <UserModal setIsOpenedUserModal={setIsOpenedUserModal}/>}
     </div>
   );
 }
