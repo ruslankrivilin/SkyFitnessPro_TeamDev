597f6c344b3fb11fba65d0edde3b7f7cb2d23416
Eugene Alyoshin <aloyolreal@gmail.com>
2024-07-16 23:36:13 +0700

    Изменены ключи для паддингов (pS, pM, pL, pXL), добавлен blockShadow, добавлены общие стили для body и некоторых элементов, добавлен контекст для применения полученных курсов на всех страницах, изменен файл courseLogoSrc на courseSettings - добавлены ссылки на картинки и цвета

--- src/types.ts
+++ src/types.ts
@@ -1,7 +1,12 @@
 export type UserType = Record<string, unknown>;
 
-export type ContextType = {
+export type UserContextType = {
   user: UserType | null;
   login: (arg: UserType) => void;
   logout: VoidFunction;
+};
+
+export type CoursesContextType = {
+  courses: string[];
+  setCourses: (arg: string[]) => void
 };
