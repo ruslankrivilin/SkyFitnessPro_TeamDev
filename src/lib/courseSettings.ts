9fd6c17f79b40eaf0ed5e0c3b8e01d35aafe3547
Eugene Alyoshin <aloyolreal@gmail.com>
2024-07-18 12:57:24 +0700

    Названия курсов на русском для использования в компонентах

--- src/lib/courseSettings.ts
+++ src/lib/courseSettings.ts
@@ -1,26 +1,26 @@
 export const courseLogoSrc = [
   {
-    courseName: "BodyFlex",
+    courseName: "Бодифлекс",
     imgSrc: "/public/images/images_small/bodyflex_female_sm.png",
     bgColor: "bodyFlexColor",
   },
   {
-    courseName: "StepAerobic",
+    courseName: "Степ-аэробика",
     imgSrc: "/public/images/images_small/step_aerobic_sm.png",
     bgColor: "stepAerobicColor",
   },
   {
-    courseName: "Stretching",
+    courseName: "Стретчинг",
     imgSrc: "/public/images/images_small/stretching_female_sm.png",
     bgColor: "stretchingColor",
   },
   {
-    courseName: "Yoga",
+    courseName: "Йога",
     imgSrc: "/public/images/images_small/yoga_female_sm.png",
     bgColor: "yogaColor",
   },
   {
-    courseName: "Zumba",
+    courseName: "Зумба",
     imgSrc: "/public/images/images_small/zumba_female_sm.png",
     bgColor: "zumbaColor",
   },
