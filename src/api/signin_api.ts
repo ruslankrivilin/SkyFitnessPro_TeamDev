5d566682724b902e5c68c7e38f04ebbcc8233c19
DKTNS <zigindmitry@gmail.com>
2024-07-20 15:57:36 +0300

    LOGIN: Добавлено получение получение курсов с прогрессом по ИД юзера. Сейчас возвращает undefine т.к. курсов нет на юзере. юзер для теста 111@mail.ru 111111

--- src/api/signin_api.ts
+++ src/api/signin_api.ts
@@ -3,6 +3,7 @@ import { setUser } from "./slices/userSlices";
 import { auth } from "./firebase_api";
 import { useNavigate } from "react-router-dom";
 import { useAppDispatch } from "../hooks/redux-hooks";
+import { getFavoriteCourseOfUser } from "./courses_api";
 
 // signinApi должна быть компонентом React, переименовл ее заглавной буквы.
 //В ином случае не работают хуки useAppDispatch и useNavigate
@@ -23,6 +24,15 @@ export default function SigninApi(email: string, password: string) {
       navigate("/");
       console.log(user);
       console.log({ email, password });
+
+      // Получение курсов, относящихся только к конкретному userId
+      getFavoriteCourseOfUser(user.uid)
+        .then((courses) => {
+          console.log(courses);
+        })
+        .catch((error) => {
+          console.error(error);
+        });
     })
     .catch(() => alert("Ошибка авторизации! Попробуйте зарегистрироваться."));
   console.log({ email, password });
