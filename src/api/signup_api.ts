a5ebf88bdc4e636daae932052343338ff677448c
aloygol <146754167+aloygolone@users.noreply.github.com>
2024-07-21 18:48:06 +0700

    Update signup_api.ts

--- src/api/signup_api.ts
+++ src/api/signup_api.ts
@@ -3,18 +3,21 @@ import { setUser } from "./slices/userSlices";
 import { useAppDispatch } from "../hooks/redux-hooks";
 import { useNavigate } from "react-router-dom";
 import { auth } from "./firebase_api";
-type api = {
-  email: string,
-  passwordFirst: string,
-  passwordSecond: string,
-}
-export default function signupApi(
-  {email, passwordFirst}: api
-) {
-  createUserWithEmailAndPassword(auth, email, passwordFirst)
+
+// signupApi должна быть компонентом React, переименовл ее заглавной буквы.
+//В ином случае не работают хуки useAppDispatch и useNavigate
+export default function SignupApi(email: string, password: string) {
+  const navigate = useNavigate();
+  const dispatch = useAppDispatch();
+  createUserWithEmailAndPassword(auth, email, password)
     .then(({ user }) => {
       console.log(user);
-      const dispatch = useAppDispatch();
+      //для теста выводимых параметров.
+      console.log("courseID:", "");
+      console.log("Progress:", "");
+      console.log("token:", user.refreshToken);
+      console.log("id:", user.uid);
+      console.log("email:", user.email);
       dispatch(
         setUser({
           email: user.email,
@@ -22,8 +25,53 @@ export default function signupApi(
           token: user.refreshToken,
         }),
       );
-      const navigate = useNavigate();
       navigate("/");
     })
     .catch(console.error);
 }
+
+
+// //---------------------------------------------
+// //Разделение функция для api & regForm
+
+// //For API
+// export const signupApi = async (email: string, password: string) => {
+//   try {
+//     const { user } = await createUserWithEmailAndPassword(auth, email, password);
+//     const dispatch = useAppDispatch();
+//     dispatch(
+//       setUser({
+//         email: user.email,
+//         id: user.uid,
+//         token: user.refreshToken,
+//       }),
+//     );
+//     navigate("/");
+//     console.log(user);
+//     console.log({ email, password });
+
+//     // Получение курсов, относящихся только к конкретному userId
+//     getFavoriteCourseOfUser(user.uid)
+//       .then((courses) => {
+//         console.log(courses);
+//       })
+//       .catch((error) => {
+//         console.error(error);
+//       });
+//   } catch (error) {
+//     console.error(error);
+//   }
+// };
+
+// //for regForm
+// import { signupApi } from "./api";
+// // Код компонента RegForm
+// // Пример использования функции signupApi
+// const handleSignup = async (email: string, password: string) => {
+//   try {
+//     await signupApi(email, password);
+//   } catch (error) {
+//     console.error("Ошибка при регистрации:", error);
+//   }
+// };
+
