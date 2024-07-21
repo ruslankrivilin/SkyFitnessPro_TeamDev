5a2164a5d3f437e82430a72b5733048ee3d97233
Eugene Alyoshin <aloyolreal@gmail.com>
2024-07-21 23:06:44 +0700

    Добавил дополнительно путь для картинок в HD

--- src/lib/courseSettings.ts
+++ src/lib/courseSettings.ts
@@ -2,26 +2,31 @@ export const courseLogoSrc = [
   {
     courseName: "Бодифлекс",
     imgSrc: "/public/images/images_small/bodyflex_female_sm.png",
+    imgSrcHD: "/public/images/images_full/bodyflex_female_hd.png",
     bgColor: "bodyFlexColor",
   },
   {
     courseName: "Степ-аэробика",
     imgSrc: "/public/images/images_small/step_aerobic_sm.png",
+    imgSrcHD: "/public/images/images_full/step_aerobic_hd.png",
     bgColor: "stepAerobicColor",
   },
   {
     courseName: "Стретчинг",
     imgSrc: "/public/images/images_small/stretching_female_sm.png",
+    imgSrcHD: "/public/images/images_full/stretching_female_hd.png",
     bgColor: "stretchingColor",
   },
   {
     courseName: "Йога",
     imgSrc: "/public/images/images_small/yoga_female_sm.png",
+    imgSrcHD: "/public/images/images_full/yoga_female_hd.png",
     bgColor: "yogaColor",
   },
   {
     courseName: "Зумба",
     imgSrc: "/public/images/images_small/zumba_female_sm.png",
+    imgSrcHD: "/public/images/images_full/zumba_female_hd.png",
     bgColor: "zumbaColor",
   },
 ];
