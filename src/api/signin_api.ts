e0290f31a654adc7bde935938e275429f09da2f7
aloygol <146754167+aloygolone@users.noreply.github.com>
2024-07-21 19:44:05 +0700

    Update signin_api.ts

--- src/api/signin_api.ts
+++ src/api/signin_api.ts
@@ -1,98 +1,134 @@
-//import React from "react";
-//import { useState } from "react";
-//import firebase from 'firebase/app';
-import "firebase/auth";
-import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
-//const authUrl = "https://spfitnesspro.firebaseapp.com";
+import { signInWithEmailAndPassword, updatePassword } from "firebase/auth";
+import { setUser } from "./slices/userSlices";
+import { auth } from "./firebase_api";
+import { useNavigate } from "react-router-dom";
+import { useAppDispatch } from "../hooks/redux-hooks";
+import { getFavoriteCourseOfUser } from "./courses_api";
 
-export type ErrorType = {
-  error: Error;
-  reset: () => void;
-};
+// signinApi должна быть компонентом React, переименовл ее заглавной буквы.
+//В ином случае не работают хуки useAppDispatch и useNavigate
+export default function SigninApi(email: string, password: string) {
+  const navigate = useNavigate();
+  const dispatch = useAppDispatch();
+  console.log({ email, password });
+  signInWithEmailAndPassword(auth, email, password)
+    .then(({ user }) => {
+      console.log(auth, email, password);
+      dispatch(
+        setUser({
+          email: user.email,
+          id: user.uid,
+          token: user.refreshToken,
+        }),
+      );
+      navigate("/");
+      console.log(user);
+      console.log({ email, password });
 
-//Авторизация
-const auth = getAuth();
-export const loginWithFirebase = async (email, password) => {
+      // Получение курсов, относящихся только к конкретному userId
+      getFavoriteCourseOfUser(user.uid)
+        .then((courses) => {
+          console.log(courses);
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    })
+    .catch(() => alert("Ошибка авторизации! Попробуйте зарегистрироваться."));
+  console.log({ email, password });
+}
+
+// //Авторизация Login c разделением на api и formPage
+
+// //Login api
+// import { signInWithEmailAndPassword } from "./path/to/firebase/auth";
+// import { auth } from "./path/to/firebase";
+// import { useAppDispatch } from "./path/to/appDispatch";
+// import { setUser } from "./path/to/userActions";
+// import { getFavoriteCourseOfUser } from "./path/to/courseApi";
+
+// export const signinApi = async (email: string, password: string) => {
+//   try {
+//     const { user } = await signInWithEmailAndPassword(auth, email, password);
+//     const dispatch = useAppDispatch();
+//     dispatch(
+//       setUser({
+//         email: user.email,
+//         id: user.uid,
+//         token: user.refreshToken,
+//       }),
+//     );
+//     navigate("/");
+//     console.log(user);
+//     console.log({ email, password });
+
+//     // Получение курсов, относящихся только к конкретному userId
+//     getFavoriteCourseOfUser(user.uid)
+//       .then((courses) => {
+//         console.log(courses);
+//       })
+//       .catch((error) => {
+//         console.error(error);
+//       });
+//   } catch (error) {
+//     alert("Ошибка авторизации! Попробуйте зарегистрироваться.");
+//   }
+// };
+// //Login form
+// import { SigninApi } from "./api";
+// // Код компонента LoginForm
+// // Пример использования функции signinApi
+// const handleSignIn = async (email: string, password: string) => {
+//   try {
+//     await signinApi(email, password);
+//   } catch (error) {
+//     console.error("Ошибка при авторизации:", error);
+//   }
+// };
+
+
+
+
+
+//Сброс пароля
+export const changePassword = async (password: string) => {
   try {
-    const userCredential = await signInWithEmailAndPassword(
-      auth,
-      email,
-      password,
-    );
-
-    const user = userCredential.user;
-    // Дополнительные действия после успешной авторизации, например, перенаправление пользователя на другую страницу
-    console.log("Пользователь успешно авторизован:", user);
-    return user;
+    if (!auth.currentUser) {
+      throw new Error("Нет авторизации");
+    }
+    await updatePassword(auth.currentUser, password);
   } catch (error) {
-    // Обработка ошибок при авторизации
-    const errorCode: ErrorType = error.code;
-    const errorMessage: ErrorType = error.message;
-    console.error(
-      "Ошибка при авторизации пользователя:",
-      errorCode,
-      errorMessage,
-    );
-    throw new Error(errorMessage);
+    if (error instanceof Error) throw new Error(error.message);
   }
 };
 
-/*   import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 
-const auth = getAuth();
-createUserWithEmailAndPassword(auth, email: string, password: string): Promise<UserCredential>
-  .then((userCredential) => {
-    return fetch(authUrl + "/login", {
-        method: "POST",
-        body: JSON.stringify({
-          email,
-          password,
-        }),
-
-    // Успешная регистрация пользователя
-    //const user = userCredential.user;
-    // Дополнительные действия после успешной регистрации, например, перенаправление пользователя на другую страницу
-    //console.log('Пользователь успешно зарегистрирован:', user);
-  })
-  .catch((error) => {
-    // Обработка ошибок при регистрации пользователя
-    const errorCode = error.code;
-    const errorMessage = error.message;
-    console.error('Ошибка при регистрации пользователя:', errorCode, errorMessage);
-    // Дополнительные действия при возникновении ошибки, например, отображение сообщения об ошибке пользователю
-  }); */
-
-/* (<div className="wrapper">
-    <div className="container-signin">
-      <div className="modal">
-        <div className="modal__block">
-          <div className="modal__ttl">
-            <h2>Вход</h2>
-          </div>
-          <form className="modal__form-login" id="formLogIn" action="#">
-            <input
-              className="modal__input"
-              type="text"
-              name="login"
-              id="formlogin"
-              placeholder="Эл. почта"
-            />
-            <input
-              className="modal__input"
-              type="password"
-              name="password"
-              id="formpassword"
-              placeholder="Пароль"
-            />
-            <button className="modal__btn-enter _hover01" id="btnEnter">
-              <a href="../main.html">Войти</a>
-            </button>
-            <div className="modal__form-group">
-              <p>Нужно зарегистрироваться?</p>
-              <a href="signup.html">Регистрируйтесь здесь</a>
-            </div>
-          </form>
-        </div>
-      </div>
-    </div>
-  </div>) */
+//---------------------------------------------------------------------
+//смена пароля для api c разделением
+//принимает пароль в качестве аргумента. 
+//Функция проверяет, есть ли текущий авторизованный пользователь (auth.currentUser), 
+//и затем вызывает функцию updatePassword для обновления пароля. Если происходит ошибка, 
+//она будет обработана и выброшено исключение с сообщением об ошибке.
+// export const changePassword = async (password: string) => {
+//   try {
+//     if (!auth.currentUser) {
+//       throw new Error("Нет авторизации");
+//     }
+//     await updatePassword(auth.currentUser, password);
+//   } catch (error) {
+//     if (error instanceof Error) throw new Error(error.message);
+//   }
+// };
+// //Смена пароля для Form
+// //handlePasswordChange вызывает changePassword с новым паролем и обрабатывает успешное изменение пароля или ошибку при изменении.
+// import { changePassword } from "./api";
+// // Код компонента LoginForm
+// // Пример использования функции changePassword
+// const handlePasswordChange = async (newPassword: string) => {
+//   try {
+//     await changePassword(newPassword);
+//     console.log("Пароль успешно изменен");
+//   } catch (error) {
+//     console.error("Ошибка при изменении пароля:", error);
+//   }
+// };
