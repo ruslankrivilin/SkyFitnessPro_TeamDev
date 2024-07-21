109f99a88fc46427d75e99285de641ee07c3e941
Illia Hryn <grinpis01@gmail.com>
2024-07-21 14:39:41 +0200

    fix shadow style

--- src/pages/WorkoutVideoPage/WorkoutVideoPage.tsx
+++ src/pages/WorkoutVideoPage/WorkoutVideoPage.tsx
@@ -1,14 +1,16 @@
-import Header from "../../components/Common/Header/Header";
-import MyProgressModal from "../../components/OtherModals/MyProgressModal/MyProgressModal";
+/* import Header from "../../components/Common/Header/Header";
+import MyProgressModal from "../../components/OtherModals/MyProgressModal/MyProgressModal"; */
 import Exercises from "../../components/OtherComponents/Exercises/Exercises";
 import { useState } from "react";
+import CourseCard from "../../components/Common/CourseCard/CourseCard";
+/* import WorkoutModal from "../../components/OtherModals/WorkoutModal/WorkoutModal"; */
 
 export default function WorkoutVideoPage() {
   const [isOpenedMyProgress, setIsOpenedMyProgress] = useState<boolean>(false);
 
   return (
     <>
-      {isOpenedMyProgress && (
+      {/* {isOpenedMyProgress && (
         <MyProgressModal setIsOpenedMyProgress={setIsOpenedMyProgress} />
       )}
       <Header />
@@ -16,15 +18,17 @@ export default function WorkoutVideoPage() {
         <div className="aspect-w-16 aspect-h-9 relative max-h-[639px] w-full max-w-[1160px] overflow-hidden rounded-lg shadow-lg">
           <iframe
             className="left-0 top-0 h-[639px] w-full"
-            src="https://cdn.pixabay.com/video/2023/01/27/148212-793717957_large.mp4" // Замените на вашу ссылку на видео
+            src="https://cdn.pixabay.com/video/2023/01/27/148212-793717957_large.mp4"
             title="Vidio training"
             frameBorder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
             allowFullScreen
           ></iframe>
         </div>
-      </div>
+      </div> */}
       <Exercises setIsOpenedMyProgress={setIsOpenedMyProgress} />
+
+      <CourseCard isMainPage={false} />
     </>
   );
 }
