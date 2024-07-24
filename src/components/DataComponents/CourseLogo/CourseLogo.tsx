85016cb071118acead651e54a45fef4120ba55a5
Igor Shipitko <garikrhip20@gmail.com>
2024-07-24 20:33:55 +0300

    course3

--- src/components/DataComponents/CourseLogo/CourseLogo.tsx
+++ src/components/DataComponents/CourseLogo/CourseLogo.tsx
@@ -16,17 +16,15 @@ export default function CourseLogo({ courseName }: CourseLogoType) {
   return (
     <>
       <div
-        className={`rounded-blockRadius container mx-auto h-[310px] bg-${matchedCourse!.bgColor} md:my-l0`}
-      >
+        className={`rounded-blockRadius container mx-auto  bg-${matchedCourse!.bgColor} md:my-l0`}>
         <div className="marker p-30 mx-auto flex h-80 items-start">
-          <div className="hidden md:block md:w-1/3 md:pr-[800px]">
+          <div className="hidden xl:block xl:w-1/3 xl:pr-[800px]">
             <h2 className="p-xl m-10 whitespace-nowrap font-defaultFont text-7xl font-normal text-white">
-              {courseName}
+              {matchedCourse!.courseName}
             </h2>
           </div>
           <div
-            className={`relative h-full w-full bg-[${matchedCourse!.imgSrcHD}]`}
-          >
+            className={`relative h-full w-full bg-[${matchedCourse!.imgSrcHD}]`}>
             <img
               className="rounded-blockRadius  ml-auto mr-auto h-full w-auto object-contain object-center"
               src={matchedCourse!.imgSrc}
--- src/lib/courseSettings.ts
+++ src/lib/courseSettings.ts
@@ -30,3 +30,6 @@ export const courseLogoSrc = [
     bgColor: "zumbaColor",
   },
 ];
+
+
+
