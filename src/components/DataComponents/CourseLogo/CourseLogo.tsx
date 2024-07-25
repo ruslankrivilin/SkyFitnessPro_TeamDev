5ff8f063f44399b144466bd6d22593e43ca56f39
Igor Shipitko <garikrhip20@gmail.com>
2024-07-25 08:24:23 +0300

    course3 игрался со стилями но работают криво

--- src/components/DataComponents/CourseLogo/CourseLogo.tsx
+++ src/components/DataComponents/CourseLogo/CourseLogo.tsx
@@ -11,20 +11,20 @@ type CourseType = {
 };
 export default function CourseLogo({ courseName }: CourseLogoType) {
   const matchedCourse: CourseType | undefined = courseLogoSrc.find(
-    (el) => el.courseName === courseName,
-  );
+    (el) => el.courseName === courseName);
   return (
     <>
       <div
-        className={`rounded-blockRadius container mx-auto  bg-${matchedCourse!.bgColor} md:my-l0`}>
+        className={`rounded-blockRadius container mx-auto md:my-l0 bg-${matchedCourse!.bgColor} `}>
         <div className="marker p-30 mx-auto flex h-80 items-start">
-          <div className="hidden xl:block xl:w-1/3 xl:pr-[800px]">
-            <h2 className="p-xl m-10 whitespace-nowrap font-defaultFont text-7xl font-normal text-white">
+          <div className=" md:w-1/3 md:pr-[800px]">
+            <h2 className="hidden p-xl m-10 whitespace-nowrap font-defaultFont text-7xl font-normal text-white md:flex "  >
               {matchedCourse!.courseName}
             </h2>
           </div>
           <div
-            className={`relative h-full w-full bg-[${matchedCourse!.imgSrcHD}]`}>
+            className={` rounded-blockRadius  h-full w-full bg-cover `}>
+            {/* bg-center bg-${matchedCourse!.imgSrc} md:bg-${matchedCourse!.imgSrcHD} */}
             <img
               className="rounded-blockRadius  ml-auto mr-auto h-full w-auto object-contain object-center"
               src={matchedCourse!.imgSrc}
@@ -35,3 +35,4 @@ export default function CourseLogo({ courseName }: CourseLogoType) {
     </>
   );
 }
+
--- src/components/OtherComponents/LowStartMan/LowStartMan.tsx
+++ src/components/OtherComponents/LowStartMan/LowStartMan.tsx
@@ -1,19 +1,20 @@
 export default function LowStartMan() {
   return (
-    <div className="absolute z-10 left-20 top-[370px] flex h-full w-full justify-around md:ml-[250px]">
-      <div className="absolute right-[120px] top-[85px] z-30 md:pr-60 md:pt-28">
-        <svg className="h-12 pb-2">
+    <div className="absolute left-16 top-[-100px] z-10 flex h-full w-full justify-around md:right-[100px] md:top-[90px] ">
+      <div className="absolute right-[110px] top-[80px] z-30 md:pr-110 md:pt-28 md:top-0 md:right-[310px]">
+        <svg className="h-12 pb-4">
           <use xlinkHref="/icons/sprite.svg#icon-arc-small" />
         </svg>
       </div>
-      <div className="z-10 h-full w-full">
+     <div className="z-10 h-full w-full top[480px] ">
+     {/* bg-[url('/images/images_full/low_start_man_hd.png')] */}
         <img
-          className="absolute object-contain object-center"
+          className="absolute object-contain object-center right-0 md:right-[100px]"
           src="/images/images_full/low_start_man_hd.png"
         />
       </div>
-      <div className="absolute right-8 top-[120px] z-0">
-        <svg className="h-[370px] w-[475px] object-contain object-center md:h-[431px] md:w-[770px]">
+      <div className="absolute right-8 top-[90px] z-0 md:top-[190px]">
+        <svg className="h-[370px] w-[475px] pl-[75px] object-contain object-center md:h-[431px] md:w-[770px] md:pl-0">
           <use xlinkHref="/icons/sprite.svg#icon-arc-big " />
         </svg>
       </div>
